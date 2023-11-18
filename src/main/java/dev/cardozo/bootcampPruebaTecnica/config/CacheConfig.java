5c212dd96d3456e517622b593fd97c8955ee60ab
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-11-18 11:09:09 -0300

    cache complete

--- src/main/java/dev/cardozo/bootcampPruebaTecnica/config/CacheConfig.java
+++ src/main/java/dev/cardozo/bootcampPruebaTecnica/config/CacheConfig.java
@@ -2,30 +2,32 @@ package dev.cardozo.bootcampPruebaTecnica.config;
 
 import java.time.Duration;
 
-import org.springframework.cache.CacheManager;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
   @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory();
+  public RedisCacheConfiguration cacheConfiguration() {
+    return RedisCacheConfiguration.defaultCacheConfig()
+        .entryTtl(Duration.ofSeconds(10))
+        .disableCachingNullValues()
+        .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
   }
 
   @Bean
-  public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-    RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-        .entryTtl(Duration.ofMinutes(1));
-    return RedisCacheManager.builder(redisConnectionFactory)
-        .cacheDefaults(redisCacheConfiguration)
-        .build();
+  public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+    return (builder) -> builder
+        .withCacheConfiguration("itemCache",
+            RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(10)))
+        .withCacheConfiguration("customerCache",
+            RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(10)));
   }
 }
--- src/main/java/dev/cardozo/bootcampPruebaTecnica/service/WeatherService.java
+++ src/main/java/dev/cardozo/bootcampPruebaTecnica/service/WeatherService.java
@@ -4,10 +4,16 @@ import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import dev.cardozo.bootcampPruebaTecnica.config.CacheConfig;
 import dev.cardozo.bootcampPruebaTecnica.dto.AirPollutionDto;
 import dev.cardozo.bootcampPruebaTecnica.dto.ForecastDto;
 import dev.cardozo.bootcampPruebaTecnica.dto.WeatherDto;
@@ -15,6 +21,11 @@ import dev.cardozo.bootcampPruebaTecnica.entities.User;
 import dev.cardozo.bootcampPruebaTecnica.entities.WeatherRequest;
 import dev.cardozo.bootcampPruebaTecnica.repositories.WeatherRepository;
 
+@Import({ CacheConfig.class })
+@ImportAutoConfiguration(classes = {
+    CacheAutoConfiguration.class,
+    RedisAutoConfiguration.class
+})
 @Service
 public class WeatherService {
 
@@ -44,21 +55,31 @@ public class WeatherService {
     return weatherDto;
   }
 
+  @Cacheable("getWeatherForecastDto")
   public ForecastDto getWeatherForecastDto(String cityName) {
-
-    String apiUrl = weatherApiUrl + "/forecast?q=" + cityName + "&appid=" + apiKey;
-    ForecastDto forecastDto = callWeatherApi(apiUrl, ForecastDto.class);
-
-    if (forecastDto != null && forecastDto.getEntries() != null && forecastDto.getEntries().size() > 5) {
-      forecastDto.setEntries(forecastDto.getEntries().subList(0, 5));
+    try {
+      String apiUrl = weatherApiUrl + "/forecast?q=" + cityName + "&appid=" + apiKey;
+      ForecastDto forecastDto = callWeatherApi(apiUrl, ForecastDto.class);
+      if (forecastDto != null && forecastDto.getEntries() != null && forecastDto.getEntries().size() > 5) {
+        forecastDto.setEntries(forecastDto.getEntries().subList(0, 5));
+      }
+      return forecastDto;
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw e;
     }
-    return forecastDto;
   }
 
+  @Cacheable("getAirPollutionData")
   public AirPollutionDto getAirPollutionData(double latitude, double longitude) {
-    String apiUrl = weatherApiUrl + "/air_pollution?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
-    AirPollutionDto airPollutionDto = callWeatherApi(apiUrl, AirPollutionDto.class);
-    return airPollutionDto;
+    try {
+      String apiUrl = weatherApiUrl + "/air_pollution?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+      AirPollutionDto airPollutionDto = callWeatherApi(apiUrl, AirPollutionDto.class);
+      return airPollutionDto;
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw e;
+    }
   }
 
   private <T> T callWeatherApi(String apiUrl, Class<T> responseType) {
